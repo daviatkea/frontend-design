2ca8e94028014592012d00ca4c0440274940173f
Dannie Vinther <davi@kea.dk>
2024-03-06 13:08:33 +0100

    add challenge sticky-labels

--- src/data/card-content.js
+++ src/data/card-content.js
@@ -1,18 +1,115 @@
-export const cardContent = [
+export const data = [
   {
-    title: "The Balance Sheet",
-    body: "The Balance Sheet is a summary of the assets and liabilities and equity of a business at a specific point of time.",
+    year: 2023,
+    months: [
+      {
+        month: "April",
+        cards: [
+          { title: "Event 1", content: "Detaljer om event 1." },
+          { title: "Event 2", content: "En kort beskrivelse af event 2." },
+          { title: "Oplevelse", content: "En historie om en unik oplevelse." },
+        ],
+      },
+      {
+        month: "May",
+        cards: [
+          { title: "Innovation", content: "Gennembrud inden for teknologi." },
+          { title: "Milepæl", content: "En vigtig milepæl nået dette år." },
+          { title: "Udforskning", content: "Nye opdagelser i det ukendte." },
+          { title: "Kunst", content: "Fremhævning af betydningsfuld kunst." },
+          { title: "Musik", content: "Betydelige musikudgivelser." },
+        ],
+      },
+      {
+        month: "September",
+        cards: [
+          {
+            title: "Videnskab",
+            content: "Banebrydende forskning og opdagelser.",
+          },
+          { title: "Teknologi", content: "Fremvæksten af nye tech trends." },
+          { title: "Rejser", content: "Opmærksomme rejsemål og eventyr." },
+          {
+            title: "Litteratur",
+            content: "Notable bogudgivelser og anmeldelser.",
+          },
+          {
+            title: "Sport",
+            content: "Væsentlige sportsbegivenheder og resultater.",
+          },
+        ],
+      },
+    ],
   },
   {
-    title: "Income Statement",
-    body: "The Income (Profit and Loss) Statement, commonly referred to as the P&L statement, summarizes the revenue and expenses for a specific time period (one month, one quarter, one year, etc.)",
+    year: 2024,
+    months: [
+      {
+        month: "January",
+        cards: [
+          { title: "Innovation", content: "Gennembrud inden for teknologi." },
+          { title: "Milepæl", content: "En vigtig milepæl nået dette år." },
+          { title: "Udforskning", content: "Nye opdagelser i det ukendte." },
+          { title: "Kunst", content: "Fremhævning af betydningsfuld kunst." },
+          { title: "Musik", content: "Betydelige musikudgivelser." },
+        ],
+      },
+      {
+        month: "February",
+        cards: [
+          { title: "Event 1", content: "Detaljer om event 1." },
+          { title: "Event 2", content: "En kort beskrivelse af event 2." },
+          { title: "Oplevelse", content: "En historie om en unik oplevelse." },
+          {
+            title: "Litteratur",
+            content: "Notable bogudgivelser og anmeldelser.",
+          },
+          {
+            title: "Sport",
+            content: "Væsentlige sportsbegivenheder og resultater.",
+          },
+        ],
+      },
+    ],
   },
   {
-    title: "Financial Plan",
-    body: "Keep your information current and review the documents on a regular basis (monthly or more often if needed). Review them with key individuals within your company.",
-  },
-  {
-    title: "Financial Ratios",
-    body: "Ratios are useful when comparing your company with the competition on financial performance and also when benchmarking the performance of your company.",
+    year: 2025,
+    months: [
+      {
+        month: "November",
+        cards: [
+          {
+            title: "Videnskab",
+            content: "Banebrydende forskning og opdagelser.",
+          },
+          { title: "Teknologi", content: "Fremvæksten af nye tech trends." },
+          { title: "Rejser", content: "Opmærksomme rejsemål og eventyr." },
+          {
+            title: "Litteratur",
+            content: "Notable bogudgivelser og anmeldelser.",
+          },
+          {
+            title: "Sport",
+            content: "Væsentlige sportsbegivenheder og resultater.",
+          },
+        ],
+      },
+      {
+        month: "December",
+        cards: [
+          {
+            title: "Litteratur",
+            content: "Notable bogudgivelser og anmeldelser.",
+          },
+          {
+            title: "Sport",
+            content: "Væsentlige sportsbegivenheder og resultater.",
+          },
+          { title: "Event 1", content: "Detaljer om event 1." },
+          { title: "Event 2", content: "En kort beskrivelse af event 2." },
+          { title: "Oplevelse", content: "En historie om en unik oplevelse." },
+        ],
+      },
+    ],
   },
 ];
